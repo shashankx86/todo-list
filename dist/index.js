ae797601f021f5910f4b8dd04cae77b18c96ce14
Shashank Shekhar Pandey <shashankspis20@gmail.com>
2024-08-07 13:29:17 +0530

    added new Todo interface and updated functions to createTodo and handleSubmit

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,26 @@
 "use strict";
 // Aproach 1 - use question mark in case btn can't be found.
-// const btn = document.getElementById("btn");
-// btn?.addEventListener("click", function() {
-//     alert("CLICKED!!!");
-// });
-// Approach 2 - use exclamation point (no-null assertion operator)
+const todos = [];
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
 const form = document.querySelector("form");
 const list = document.getElementById("todolist");
 function handleSubmit(e) {
     e.preventDefault();
-    const newTodoText = input.value;
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    input.value = "";
+}
+function createTodo(todo) {
     const newLI = document.createElement("LI");
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    newLI.append(newTodoText);
+    newLI.append(todo.text);
     newLI.append(checkbox);
     list.append(newLI);
-    input.value = "";
 }
 form.addEventListener("submit", handleSubmit);
